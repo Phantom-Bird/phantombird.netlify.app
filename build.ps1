b67891d13ce4712f20dc006953b6315001effed9
Phantom-Bird <96868057+Phantom-Bird@users.noreply.github.com>
2024-11-09 22:38:12 +0800

    asciiized build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 pnpm docs:build
 copy netlify.toml docs/.vuepress/dist
 # pnpm docs:preview
-echo "请在 git 中提交更改，或按 Ctrl-C 退出"
+echo "Commit changes in git, or press Ctrl-C to exit."
 pause
 git push origin master
