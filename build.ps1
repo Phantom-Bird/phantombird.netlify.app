62b96875d5a67eb910ffa67871e560d2db5f452e
Phantom-Bird <96868057+Phantom-Bird@users.noreply.github.com>
2024-11-10 08:53:37 +0800

    upd FFMP-20241108; upd build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,13 @@
 pnpm docs:build
 copy netlify.toml docs/.vuepress/dist
-# pnpm docs:preview
-echo "Commit changes in git and continue, or press Ctrl-C to exit."
-pause
-git push origin master
+
+# type anything else means no
+$response = Read-Host "Push to github? (Y/n) Plz commit first."
+if ($response -eq "Y" -or $response -eq "y" -or $response -eq ""){
+    git push origin master
+}
+
+$response = Read-Host "Preview? (y/N)"
+if ($response -eq "Y" -or $response -eq "y"){
+    pnpm docs:preview
+}
